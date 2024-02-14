88ce0ac4b6adb86dc603839165072bd5172b834d
fabiliima <fabilimaj@hotmail.com>
2024-02-14 15:08:42 -0300

    created content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,6 @@
+export const dataFake = [{
+  'id': '1',
+  'title': 'isso é um teste',
+  'description': 'olá',
+  'photoCover': 'https://cordierinvestimentos.com.br/wp-content/uploads/2020/09/placeholder.png'
+}]
